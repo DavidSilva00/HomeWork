857a8e5ba6b2d2c986ffdb06147c8eaa27bee9f9
David Silva <david.rob.silva1@gmail.com>
2020-09-18 15:08:19 -0400

    This is my fourth  Git commit

--- UserInfo.java
+++ UserInfo.java
@@ -1,5 +1,5 @@
 public class UserInfo{
 	public static void main(String[] args){
-		System.out.println("I am taking CS407 because I want to help less fortunate people with my code/ make a decent living");
+		System.out.println("My favoite food is steak");
 	}
 }
